628fb685e6db718c8272b5a503c4c843d0b08f5c
Valdir2373 <valdirdesouzajunioradm@gmail.com>
2025-03-23 06:05:57 -0300

    configurando import do DB .ts para .js

--- src/back-end/config/DB.ts
+++ src/back-end/config/DB.ts
@@ -1,4 +1,4 @@
-import envConfig from "./env.config";
+import envConfig from "./env.config.js";
 import { neon } from "@neondatabase/serverless";
 envConfig;
 export const sql = neon(process.env.DATABASE_URL!);
